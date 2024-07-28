a17e287d48f8a4c0c8de80dc6735cd99270c874b
Daniel Trubač <trubacd@elektlabs.cz>
2024-07-28 09:55:32 +0200

    Show and refresh timer

--- App/ViewModels/MainViewModel.cs
+++ App/ViewModels/MainViewModel.cs
@@ -1,6 +1,31 @@
-﻿namespace App.ViewModels;
+﻿using System;
+using System.Threading;
+
+namespace App.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private string fText;
+    private Timer fTimer;
+
+    public MainViewModel()
+    {
+        fText = "Welcome to Avalonia!";
+        fTimer = new Timer(this.Tick, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(10));
+    }
+
+    public string Greeting
+    {
+        get => fText;
+        private set
+        {
+            fText = value;
+            OnPropertyChanged(nameof(Greeting));
+        }
+    }
+
+    private void Tick(object? state)
+    {
+        Greeting = DateTime.Now.ToString("HH:mm:ss.f");
+    }
 }
